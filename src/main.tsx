53a38319c96e1812fe5835348d83b9e2e4cff19f
Adriel Gonzalo Barrientos Mercado <122940027+Adrielgbm@users.noreply.github.com>
2025-05-23 21:37:04 -0400

    feat: add UI components by shadCN and improve project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/globals.css";
 
-createRoot(document.getElementById('root')!).render(
+import Home from "./app/Home.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Home />
+  </StrictMode>
+);
